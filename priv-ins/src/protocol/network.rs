f9bdffcec5e1e056788d8cbc0787bf5f815ae44c
Jan Koscisz <koscis.j@gmail.com>
2022-02-12 14:10:15 +0100

    x

--- priv-ins/src/protocol/network.rs
+++ priv-ins/src/protocol/network.rs
@@ -3,13 +3,14 @@ use crate::protocol::NodeId;
 pub enum Msg {
 
 }
+
 pub type NetworkMessage = (NodeId, Msg);
 
 #[async_trait::async_trait]
 trait Network {
     async fn send_to(msg: NetworkMessage);
     async fn receive() -> NetworkMessage;
-    async fn broadcast();
+    async fn broadcast(msg: Msg);
 }
 
 
